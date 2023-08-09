a073e4db4dfd40caec9850678239c353343b5d2d
Victorcalp <victorcalp@outlook.com.br>
2023-08-09 14:23:10 -0300

    Adicionado try\catch em CategoryController

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -19,50 +19,97 @@ namespace BlogAPI.Controllers
         [HttpGet("v1/categories")]
         public async Task<IActionResult> GetAsync()
         {
-            var categories = await _context.Categories.ToListAsync();
-            return Ok(categories);
+            try
+            {
+                var categories = await _context.Categories.ToListAsync();
+                return Ok(categories);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
 
         [HttpGet("v1/categories/{id:int}")]
         public async Task<IActionResult> GetByIdAsync(int id)
         {
-            var category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
-            if (category == null) return NotFound();
+            try
+            {
+                var category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+                if (category == null) return NotFound();
 
-            return Ok(category);
+                return Ok(category);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
 
         [HttpPost("v1/categories")]
         public async Task<IActionResult> PostAsync(Category category)
         {
-            await _context.Categories.AddAsync(category);
-            await _context.SaveChangesAsync();
-            return Created($"v1/categories/{category.Id}", category);
+            try
+            {
+                await _context.Categories.AddAsync(category);
+                await _context.SaveChangesAsync();
+                return Created($"v1/categories/{category.Id}", category);
+            }
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(500, "05x01 - Não foi possivel incluir a categoria - " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "05x02 - Erro interno: " + ex.Message);
+            }
         }
 
-        [HttpPut("v1/categories/id")]
+        [HttpPut("v1/categories/{id}")]
         public async Task<IActionResult> PutAsync(int id, Category model)
         {
-            var categories = await _context.Categories.FirstOrDefaultAsync(x => x.Id == id);
-            if (categories == null) return NotFound();
+            try
+            {
+                var categories = await _context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+                if (categories == null) return NotFound();
 
-            categories.Name = model.Name;
-            categories.Slug = model.Slug;
+                categories.Name = model.Name;
+                categories.Slug = model.Slug;
 
-            _context.Categories.Update(categories);
-            await _context.SaveChangesAsync();
-            return Ok(model);
+                _context.Categories.Update(categories);
+                await _context.SaveChangesAsync();
+                return Ok(model);
+            }
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(500, "05x03 - Não foi possivel atualizar a categoria - " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "05x04 - Erro interno - " + ex.Message);
+            }
         }
 
-        [HttpDelete("v1/categories/id")]
+        [HttpDelete("v1/categories/{id}")]
         public async Task<IActionResult> DeleteAsync(int id)
         {
-            var categories = await _context.Categories.FirstOrDefaultAsync(x => x.Id == id);
-            if (categories == null) return NotFound();
+            try
+            {
+                var categories = await _context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+                if (categories == null) return NotFound();
 
-            _context.Categories.Remove(categories);
-            await _context.SaveChangesAsync();
-            return Ok(categories);
+                _context.Categories.Remove(categories);
+                await _context.SaveChangesAsync();
+                return Ok(categories);
+            }
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(500, "05x05 - Não foi possivel excluir a categoria - " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "05x06 - Erro interno - " + ex.Message);
+            }
         }
     }
 }
