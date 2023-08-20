803e16d3036f089eb3696402636fd6a9e739045d
Victorcalp <victorcalp@outlook.com.br>
2023-08-19 23:30:51 -0300

    Organizando Program

--- Program.cs
+++ Program.cs
@@ -7,46 +7,63 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
-//informa o esquema de autenticação
-builder.Services.AddAuthentication(x =>
-{
-    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(x =>
-{
-    //informa como desencriptar esse token e como vai lidar 
-    x.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true, //valida a chave da assinatura 
-        IssuerSigningKey = new SymmetricSecurityKey(key), //atraves da chave assimetrica
-        ValidateIssuer = false,
-        ValidateAudience = false
-    };
-});
-
-builder.Services.AddControllers();
-builder.Services.AddDbContext<Context>();
-
-builder.Services.AddTransient<TokenService>();
-
-//Configura o comportamento da API para desabilitar o filtro de ModelState do ASP.NET (validação automatica)
-builder.Services.AddControllers().ConfigureApiBehaviorOptions(options => options.SuppressModelStateInvalidFilter = true);
+ConfigureAuthentication(builder);
+ConfigureMvc(builder);
+ConfigureServices(builder);
 
 var app = builder.Build();
+
+LoadConfiguration(app);
 
 //informa que precisa usar a autenticação e autorização
 //sempre nessa ordem
 app.UseAuthentication();
 app.UseAuthorization();
 
-//Carrega os valores de configuração
-Configuration.JwtKey = app.Configuration.GetValue<string>("JwtKey");
-
-var smtp = new Configuration.SmtpConfiguration();
-app.Configuration.GetSection("SmtpConfiguration").Bind(smtp);
-Configuration.Smtp = smtp;
-
 app.MapControllers();
 
 app.Run();
+
+void LoadConfiguration(WebApplication app)
+{
+    //Carrega os valores de configuração
+    Configuration.JwtKey = app.Configuration.GetValue<string>("JwtKey");
+
+    var smtp = new Configuration.SmtpConfiguration();
+    app.Configuration.GetSection("SmtpConfiguration").Bind(smtp); //.bind pega o JSON e converte para a classe smtp
+    Configuration.Smtp = smtp;
+}
+
+void ConfigureAuthentication(WebApplicationBuilder builder)
+{
+    var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+    //informa o esquema de autenticação
+    builder.Services.AddAuthentication(x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(x =>
+    {
+        //informa como desencriptar esse token e como vai lidar 
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true, //valida a chave da assinatura 
+            IssuerSigningKey = new SymmetricSecurityKey(key), //atraves da chave assimetrica
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+}
+
+void ConfigureMvc(WebApplicationBuilder builder)
+{
+    //Configura o comportamento da API para desabilitar o filtro de ModelState do ASP.NET (validação automatica)
+    builder.Services.AddControllers().ConfigureApiBehaviorOptions(options => options.SuppressModelStateInvalidFilter = true);
+}
+
+void ConfigureServices(WebApplicationBuilder builder)
+{
+    builder.Services.AddControllers();
+    builder.Services.AddDbContext<Context>();
+    builder.Services.AddTransient<TokenService>();
+}
